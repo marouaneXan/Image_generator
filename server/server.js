e777be241fbda4c0c5d481d6b840562decd2f08e
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-26 01:11:11 +0100

    run the server at the port 5000

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//run server
+app.listen(PORT, () => console.log("Server running at the PORT " + PORT));

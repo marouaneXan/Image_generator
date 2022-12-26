b1a77dd4243d37060530a64d8b11b389c386edf9
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-26 01:54:04 +0100

    set the configuration for oprnai and create a function generateImage

--- server/Routes/openAiRoutes.js
+++ server/Routes/openAiRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router=express.Router()
+const {generateImage}=require('../Controller/openAiController')
 
-router.post('/generateImage',(req,res)=>{
-    res.send("Image generated")
-})
+router.post('/generateImage',generateImage)
 
 module.exports=router
